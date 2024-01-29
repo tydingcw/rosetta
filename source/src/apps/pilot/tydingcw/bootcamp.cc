c8868e4eb49626697e237bd40817f8559274dca2
tydingcw <claiborne.w.tydings@vanderbilt.edu>
2024-01-29 17:33:07 -0600

    added random number and phi psi functionality

--- source/src/apps/pilot/tydingcw/bootcamp.cc
+++ source/src/apps/pilot/tydingcw/bootcamp.cc
@@ -17,7 +17,8 @@
 #include <core/scoring/ScoreFunction.fwd.hh>
 #include <core/scoring/ScoreFunctionFactory.hh>
 #include <core/scoring/ScoreFunction.hh>
-#include <numeric/random.hh>
+#include <numeric/random/random.hh>
+#include <protocols/moves/MonteCarlo.hh>
 
 int main( int argc, char ** argv ) {
 	std::cout << "Hello World!" << std::endl;
@@ -35,6 +36,27 @@ int main( int argc, char ** argv ) {
 	core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function();
 	core::Real score = sfxn->score( *mypose );
 	std::cout << score << std::endl;
+
+	core::Size n_resi = mypose->size();
+
+	//core::Real gauss = numeric::random::gaussian();
+	//std::cout << gauss << std::endl;
+
+	//core::Real uni = numeric::random::uniform();
+	//std::cout << uni << std::endl;
+
+	core::Size randres = n_resi * uni;
+	std::cout << randres << " randres"  << std::endl;
+
+	core::Real pert1 = numeric::random::gaussian();
+	core::Real pert1 = numeric::random::gaussian();
+
+	core::Real orig_phi = mypose->phi( randres );
+	core::Real orig_psi = mypose->psi( randres );
+	mypose->set_phi( randres, orig_phi + pert1 );
+	mypose->set_psi( randres, orig_psi + pert2 );
+
+
 	return 0;
 }
 
