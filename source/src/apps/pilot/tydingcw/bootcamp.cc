ed7c1135aa695fdcd73b7cfc59179b289ad42f37
tydingcw <claiborne.w.tydings@vanderbilt.edu>
2024-01-29 20:37:40 -0600

    adding packer and boltzmann

--- source/src/apps/pilot/tydingcw/bootcamp.cc
+++ source/src/apps/pilot/tydingcw/bootcamp.cc
@@ -19,8 +19,12 @@
 #include <core/scoring/ScoreFunction.hh>
 #include <numeric/random/random.hh>
 #include <protocols/moves/MonteCarlo.hh>
+#include <protocols/moves/PyMOLMover.hh>
 
 int main( int argc, char ** argv ) {
+
+	using namespace protocols::moves;
+
 	std::cout << "Hello World!" << std::endl;
 
 	devel::init( argc, argv );
@@ -42,20 +46,37 @@ int main( int argc, char ** argv ) {
 	//core::Real gauss = numeric::random::gaussian();
 	//std::cout << gauss << std::endl;
 
-	//core::Real uni = numeric::random::uniform();
+	core::Real uni = numeric::random::uniform();
 	//std::cout << uni << std::endl;
 
-	core::Size randres = n_resi * uni;
-	std::cout << randres << " randres"  << std::endl;
+	//core::Size randres = n_resi * uni + 1;
+	//std::cout << randres << " randres"  << std::endl;
 
+
+	MonteCarlo monte_carlo = MonteCarlo(*mypose, *sfxn, 0.6);
+
+	protocols::moves::PyMOLObserverOP the_observer = protocols::moves::AddPyMOLObserver( *mypose, true, 0 );
+	the_observer->pymol().apply( *mypose);
+
+	for (int i = 0; i < 100; i++){
+
+	core::Size randres = n_resi * uni + 1;
 	core::Real pert1 = numeric::random::gaussian();
-	core::Real pert1 = numeric::random::gaussian();
+	core::Real pert2 = numeric::random::gaussian();
 
 	core::Real orig_phi = mypose->phi( randres );
 	core::Real orig_psi = mypose->psi( randres );
 	mypose->set_phi( randres, orig_phi + pert1 );
 	mypose->set_psi( randres, orig_psi + pert2 );
 
+	core::pack::task::PackerTaskOP repack_task = core::pack::task::TaskFactory::create_packer_task( *mypose );
+	repack_task->restrict_to_repacking();
+	core::pack::pack_rotamers( *mypose, *sfxn, repack_task );
+
+	
+	monte_carlo.boltzmann(*mypose);
+		
+	}
 
 	return 0;
 }
