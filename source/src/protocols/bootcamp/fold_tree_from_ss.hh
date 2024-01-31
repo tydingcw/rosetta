5c3cca49f9acabd2bc73b3aef3fa910c9dc7a240
tydingcw <claiborne.w.tydings@vanderbilt.edu>
2024-01-30 20:31:31 -0600

    created new files for foldtree

---
+++ source/src/protocols/bootcamp/fold_tree_from_ss.hh
@@ -0,0 +1,38 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file   test/protocols/match/ProteinSCSampler.cxxtest.hh
+/// @brief
+/// @author Andrew Leaver-Fay (aleaverfay@gmail.com)
+
+
+#include <test/util/pose_funcs.hh>
+#include <test/core/init_util.hh>
+
+// Utility headers
+#include <core/kinematics/FoldTree.hh>
+
+/// Project headers
+//#include <core/types.hh>
+#include <core/scoring/dssp/Dssp.hh>
+#include <protocols/moves/DsspMover.hh>
+
+// C++ headers
+#include <utility/vector1.hh>
+#include <string>
+#include <iostream>
+
+utility::vector1< std::pair< core::Size, core::Size > >
+identify_secondary_structure_spans( std::string const & ss_string ){}
+
+core::kinematics::FoldTree fold_tree_from_dssp_string(
+        std::string input_string
+) {}
+
+core::kinematics::FoldTree fold_tree_from_ss(core::pose::Pose & pose) {}
